a6bc8457b619c95af3b4f2339d17ca9538a53e16
Christopher Petro <cpetro@kcg.com>
2014-06-20 13:42:43 -0400

    Added option to run parallel workers in independent working directories, to allow tests to be parallelized even if they create files on the file system
    Signed-off-by: suttona <suttona@lmax.com>

--- src/main/java/com/lmax/ant/paralleljunit/remote/controller/RemoteTestRunnerProcessFactory.java
+++ src/main/java/com/lmax/ant/paralleljunit/remote/controller/RemoteTestRunnerProcessFactory.java
@@ -15,17 +15,16 @@
  */
 package com.lmax.ant.paralleljunit.remote.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
 import com.lmax.ant.paralleljunit.ParallelJUnitTaskConfig;
 import com.lmax.ant.paralleljunit.remote.process.RemoteTestRunner;
 import com.lmax.ant.paralleljunit.util.process.DelegatingProcessBuilder;
 import com.lmax.ant.paralleljunit.util.process.ProcessBuilderFactory;
-
 import org.apache.tools.ant.BuildException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class RemoteTestRunnerProcessFactory
 {
@@ -42,9 +41,13 @@ public class RemoteTestRunnerProcessFactory
 
         final DelegatingProcessBuilder processBuilder = processBuilderFactory.createProcessBuilder(jvmCommand);
 
-        final File workingDirectory = config.getDirectory();
+        final File workingDirectory = config.getDirectory(workerId);
         if (workingDirectory != null)
         {
+            if (!workingDirectory.mkdirs() && !workingDirectory.isDirectory())
+            {
+                throw new BuildException("Failed to create worker directory: " + workingDirectory.getPath());
+            }
             processBuilder.directory(workingDirectory);
         }
 
